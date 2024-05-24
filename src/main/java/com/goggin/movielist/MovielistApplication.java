4067d0d2110acf27b90fc464a4d948631a7cd156
JackHenry-G <Jack_HG@outlook.com>
2024-05-25 09:16:43 +1000

    Remove unnecssary imports

--- src/main/java/com/goggin/movielist/MovielistApplication.java
+++ src/main/java/com/goggin/movielist/MovielistApplication.java
@@ -3,9 +3,6 @@ package com.goggin.movielist;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-import org.springframework.context.annotation.PropertySource;
 
 import lombok.extern.slf4j.Slf4j;
 
